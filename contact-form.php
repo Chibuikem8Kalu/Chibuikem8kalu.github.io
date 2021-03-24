a12196513a3a5dba395ad529fdbc7fa168eb13e0
Chibuikem8Kalu <67438668+Chibuikem8Kalu@users.noreply.github.com>
2021-03-24 16:47:35 +0100

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -2,11 +2,10 @@
 
 if (isset($_POST['submit'])) {
 	$theMail =$_POST['email'];
-	$myName = $_POST['name']
 
     $mailTo = "chibuikemkalu466@gmail.com";
     $header = "From: The Interact Website";
-    $txt = "My Name is ".$myName" I would like to subscribe to your monthly newsletter.\n This is my email address".$theMail;
+    $txt = "Hi I am a User of the Interact site.\n I would like to subscribe to your monthly newsletter.\n This is my email address".$theMail;
 	mail($mailTo, $theMail, $txt);
 	header("Location:index.html?mailsend");
 }
